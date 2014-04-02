c421952c21d63c3cfbdbe367e9abff0ebaee6baa
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2014-04-02 13:01:03 +0200

    Fix misspellings of the name in the manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,16 @@
-class puppet-yum-plugin {
+class yum-plugin-puppet {
     file { "/etc/yum/pluginconf.d/puppet.conf":
         mode    => 0644,
         owner   => root,
         group   => root,
         ensure  => file,
-        source  => "puppet:///modules/yum/puppet.conf",
+        source  => "puppet:///modules/yum-plugin-puppet/puppet.conf",
     }
     file { "/usr/lib/yum-plugins/puppet.py":
         mode    => 0644,
         owner   => root,
         group   => root,
         ensure  => file,
-        source  => "puppet:///modules/yum/puppet.py",
+        source  => "puppet:///modules/yum-plugin-puppet/puppet.py",
     }
 }
